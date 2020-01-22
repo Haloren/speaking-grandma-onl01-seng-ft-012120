7f9ab8f534834ed75f9c3088de36887097145f24
Kyle McKenna <mckennakyler@gmail.com>
2020-01-22 22:24:58 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,1 +1,8 @@
-if speak_to_grandma == 
+speak_to_grandma = phrase
+if speak_to_grandma(phrase) == phrase.downcase
+  return "HUH?! SPEAK UP, SONNY!"
+elsif speak_to_grandma(phrase) == "I LOVE YOU GRANDMA!"
+  return "I LOVE YOU TOO PUMPKIN!"
+else speak_to_grandma(phrase) == phrase.upcase
+  return "NO, NOT SINCE 1938!"
+end
